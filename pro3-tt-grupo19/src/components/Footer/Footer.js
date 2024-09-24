bba1a670f816ce46488305f311c8cee28046b811
theogolova <tgolova@udesa.edu.ar>
2024-09-24 18:44:17 -0300

    footer

--- pro3-tt-grupo19/src/components/Footer/Footer.js
+++ pro3-tt-grupo19/src/components/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
-import "./Footer.css"
+import "./Footer.css";
 
-function Footer(){
-    return(
-        <footer className="footer">
-            Felipe Maison, Theo Golova
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <span>Felipe Maison</span>
+        <span>Theo Golova</span>
+        <span>Lucas Caffarone</span>
+      </div>
     </footer>
-    )
+  );
 }
 
 export default Footer;
